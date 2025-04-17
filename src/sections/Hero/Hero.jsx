577b324a4811347d4ada20d4b93291f3785c0f65
Luffy-456 <anshuman567.in@gmail.com>
2025-04-17 18:19:50 +0530

    cleaned up & set-up the essential files

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import styles from './Hero.module.css'
+function Hero() {
+  return (
+    <section id="hero">
+        <div>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Hero
