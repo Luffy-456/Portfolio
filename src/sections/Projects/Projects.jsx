74585d0990c4df55ff0d5a966916731190c61633
Luffy-456 <anshuman567.in@gmail.com>
2025-04-18 16:00:24 +0530

    fixed some issues in app.css and created a section to showcase the projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import styles from './ProjectsStyles.module.css';
+// create and add ttwoImg
+import ttwoImg from '../../assets/viberr.png';
+import ProjectsCard from '../../common/ProjectsCard';
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <section id='projects' className={styles.container}>
+      
+     <h1 className ="sectionTitle">
+          Projects
+     </h1>
+
+     <div className = {styles.projectsContainer}>
+          <ProjectsCard 
+            src={ttwoImg}
+            link="https://github.com/Luffy-456/TTWO-Stock-Analysis-and-Prediction"
+            h3="TTWO Stock Analysis and Prediction"
+            p="Machine Learning Project for Stock Analysis & Prediction"
+          />
+     </div>
+    </section>
+  );
 }
 
 export default Projects
