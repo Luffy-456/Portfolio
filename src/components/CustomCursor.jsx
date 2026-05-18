770bee40b4ae4b90d19950f107feb602c63c433a
Luffy-456 <anshuman567.in@gmail.com>
2026-05-18 16:07:41 +0530

    custom cursor

---
+++ src/components/CustomCursor.jsx
@@ -0,0 +1,39 @@
+// components/CustomCursor.jsx
+import { useEffect, useState } from "react";
+
+export const CustomCursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const moveCursor = (e) => {
+      setPosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
+  }, []);
+
+  return (
+    <>
+      <div
+        className="cursor"
+        style={{
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+
+      <div
+        className="cursor-tail"
+        style={{
+          transform: `translate(${position.x - 10}px, ${position.y - 10}px)`,
+        }}
+      />
+    </>
+  );
+};
