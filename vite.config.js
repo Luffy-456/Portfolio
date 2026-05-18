f8cb33ca5eb303ef9bf8fe1ef02a94b85b1f4c3b
Luffy-456 <anshuman567.in@gmail.com>
2026-05-18 11:17:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  base: '/Portfolio/',
+
   resolve: {
     alias: {
-      "@" : path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
